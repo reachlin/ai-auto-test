82cb4a70e45f49d1c143a8de97c509d5dd9b0722
lin.cai <lin.cai@chowbus.com>
2024-03-20 16:10:19 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# ai-auto-test
+# ai-auto-test: Use PaddlePaddle OCR for test automation
+
+We can use pyautogui to control the keyboard and mouse for test automation. But it is hard to find the right position of a button or text input box, especially, when GUI changes a lot from version to verson. Some browser based test automation frameworks will inject a piece of javascript code to search and find intented elements, and the use case of this method is quite limited.
+
+In this solution, we send the screenshot image to an OCR engine. With each text block recognized with its borders, we can use pyautogui to test both browser based or native applications.
+
+[pyautogui](https://pyautogui.readthedocs.io/en/latest/mouse.html#mouse-scrolling)
+[PaddlePaddle OCR](https://github.com/PaddlePaddle/PaddleOCR)
---
+++ ai_auto_test.py
@@ -0,0 +1,89 @@
+# !pip install pyautogui python-dotenv
+# !pip install paddlepaddle -i https://mirror.baidu.com/pypi/simple
+# !pip install "paddleocr>=2.0.1"
+
+import pyautogui
+from PIL import Image
+from paddleocr import PaddleOCR
+
+DEBUG = False
+
+def scan_image(image):
+    ocr = PaddleOCR(
+        use_angle_cls=True,
+        lang="en",
+        show_log=False,
+        # det_max_side_len=2000,
+        # max_text_length=200,
+        ocr_version="PP-OCRv4"
+    )  # need to run only once to download and load model into memory
+    result = ocr.ocr(image, cls=True)
+    # for idx in range(len(result)):
+    #     res = result[idx]
+    #     for line in res:
+    #         print(line)
+    return result
+
+def scan_screen():
+    from datetime import datetime
+    ts = int(datetime.utcnow().timestamp())
+    
+    # Take screenshot
+    screenshot = pyautogui.screenshot()
+    
+    # Save the image
+    screenshot_file = f"./screen{ts}.png"
+    screenshot.save(screenshot_file)
+    print(screenshot_file)
+    image = Image.open(screenshot_file)
+    w, h = image.size
+    
+    # send to google for OCR to find the key word
+    #pyautogui.moveTo(30, 30, 3)
+    document = scan_image(screenshot_file)
+    if document and len(document)>0:
+        return w, h, document[0]
+    return None
+
+def find_and_click(to_find, w=0, h=0, document=None, after=None):
+    layout = find_and_move_to(to_find, w, h, document, after)
+    if layout:
+        pyautogui.click()
+    return layout
+
+def find_and_move_to(to_find, w=0, h=0, document=None, after=None):
+    if document is None:
+        w, h, document = scan_screen()
+    layout = find_tokens(document, to_find, after)
+    if layout:
+        index, x, y, _ = layout
+        sw, sh = pyautogui.size()
+        cx = x*sw/w
+        cy = y*sh/h
+        pyautogui.moveTo(cx, cy, 2)
+        return index, cx, cy
+    return None
+
+import re
+def find_tokens(document, to_find, after=None):
+    index = 0
+    for item in document:
+        text, score = item[1]
+        if DEBUG:
+            print(f"{score} {text}")
+        if score > 0.8:
+            match = re.search(to_find, text)
+            if match:
+                print("Match Found: ", item)
+                p0x, p0y = item[0][0]
+                p1x, p1y = item[0][1]
+                p2x, p2y = item[0][2]
+                x = p0x + (p1x - p0x)/3
+                y = p0y + (p2y - p0y)/3
+                if after:
+                    if index > after:
+                        return index, x, y, match.group()
+                else:
+                    return index, x, y, match.group()
+        index += 1
+    return None
